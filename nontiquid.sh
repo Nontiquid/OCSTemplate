48ce0ed8416d5b65df05924b8911a1e2a991fae1
NontiquidTV <50020588+NontiquidTV@users.noreply.github.com>
2019-09-21 23:05:11 +0800

    Update nontiquid.sh
    new

--- nontiquid.sh
+++ nontiquid.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-echo "Installing FUCK SHET "
-sudo apt-get install git
-git clone https://github.com/shadow046/lock-multilogin-vpn.git
-cp lock-multilogin-vpn/kill_multi /usr/local/sbin/
-chmod +x /usr/local/sbin/kill_multi
-sed -i '/management.*/d' /etc/openvpn/server.conf
-echo 'status openvpn-status.log ' >> /etc/openvpn/server.conf
-service openvpn restart
-echo "Potangina"
+echo "Removing Old Theme"
+rm -rf /home/panel/panelv2/asset
+rm -rf /home/panel/panelv2/view
+rm -rf /home/panel/panelv2/tmp/*
+echo "Installing RoG VPN Theme"
+cd ~
+mkdir M4rshall
+cd M4rshall
+rm -rf *
+wget https://www.dropbox.com/s/0828vemag3d6t3y/RoGVPN.tgz && tar zxvf RoGVPN.tgz
+mv asset /home/panel/panelv2
+mv view /home/panel/panelv2
+echo "Enjoy using RoG VPN Theme -Nontiquid"
