0efc4af7930107d233d040755e7b2c226102aa37
NontiquidTV <50020588+NontiquidTV@users.noreply.github.com>
2019-09-25 17:30:20 +0800

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Removing OLD Theme"
-rm -rf /home/panel/panelv5/asset
-rm -rf /home/panel/panelv5/view
-rm -rf /home/panel/panelv5/tmp/*
+rm -rf /home/panel/html/asset
+rm -rf /home/panel/html/view
+rm -rf /home/panel/htmlhtml/tmp/*
 echo "Installing SMLVPN Theme"
 cd ~
 mkdir Nontiquid
 cd Nontiquid
 rm -rf *
 wget https://www.dropbox.com/s/4tq869gaz785uxf/Nontiquid.tgz && tar zxvf Nontiquid.tgz
-mv asset /home/panel/panelv5
-mv view /home/panel/panelv5
+mv asset /home/panel/htmlhtml
+mv view /home/panel/html
 echo "Enjoy using SMLVPN Theme -Nontiquid"
