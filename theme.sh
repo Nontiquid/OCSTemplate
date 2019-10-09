372e0f0c8c080cb249f64120efc45d68d13ac03b
Nontiquid <50020588+NontiquidTV@users.noreply.github.com>
2019-10-09 15:22:27 +0800

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -3,12 +3,12 @@ echo "Removing OLD Theme"
 rm -rf /home/panel/html/asset
 rm -rf /home/panel/html/view
 rm -rf /home/panel/htmlhtml/tmp/*
-echo "Installing SMLVPN Theme"
+echo "Installing DaksVPN Theme"
 cd ~
 mkdir Nontiquid
 cd Nontiquid
 rm -rf *
-wget https://www.dropbox.com/s/4tq869gaz785uxf/Nontiquid.tgz && tar zxvf Nontiquid.tgz
+wget https://www.dropbox.com/s/ol1f9j0dp8fkjac/daks.tgz && tar zxvf daks.tgz
 mv asset /home/panel/html
 mv view /home/panel/html
-echo "Enjoy using SMLVPN Theme -Nontiquid"
+echo "Enjoy using DaksVPN Theme -Nontiquid"
