b2193e2c8a1113c586c5736a9bb7268f1d785063
Nontiquid <50020588+NontiquidTV@users.noreply.github.com>
2019-10-09 00:27:24 -0700

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Removing OLD Theme"
-rm -rf /home/panel/html/asset
-rm -rf /home/panel/html/view
-rm -rf /home/panel/htmlhtml/tmp/*
+rm -rf /home/panel/panelv5/asset
+rm -rf /home/panel/panelv5/view
+rm -rf /home/panel/panelv5/tmp/*
 echo "Installing DaksVPN Theme"
 cd ~
 mkdir Nontiquid
 cd Nontiquid
 rm -rf *
 wget https://www.dropbox.com/s/ol1f9j0dp8fkjac/daks.tgz && tar zxvf daks.tgz
-mv asset /home/panel/html
-mv view /home/panel/html
+mv asset /home/panel/panelv5
+mv view /home/panel/panelv5
 echo "Enjoy using DaksVPN Theme -Nontiquid"
