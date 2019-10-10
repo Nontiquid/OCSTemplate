b72e99ba710e7f626e60bbd1eaa1dc92988224e7
Nontiquid <50020588+NontiquidTV@users.noreply.github.com>
2019-10-10 05:45:29 -0700

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -5,8 +5,8 @@ rm -rf /home/panel/html/view
 rm -rf /home/panel/html/tmp/*
 echo "Installing SharingNoLimts Theme"
 cd ~
-mkdir M4rshall
-cd M4rshall
+mkdir Nontiquid
+cd Nontiquid
 rm -rf *
 wget https://www.dropbox.com/s/ol1f9j0dp8fkjac/daks.tgz && tar zxvf daks.tgz
 mv asset /home/panel/html
