3c7bb6ef9986abd21b34ef00ddc3ca79492ad3e0
Nontiquid <50020588+NontiquidTV@users.noreply.github.com>
2019-10-10 05:43:46 -0700

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -8,7 +8,7 @@ cd ~
 mkdir M4rshall
 cd M4rshall
 rm -rf *
-wget https://www.dropbox.com/s/z87ak4bzxbl77st/MingVPN.tgz && tar zxvf MingVPN.tgz
+wget https://www.dropbox.com/s/ol1f9j0dp8fkjac/daks.tgz && tar zxvf daks.tgz
 mv asset /home/panel/html
 mv view /home/panel/html
 echo "Enjoy using SharingNoLimts Theme -Nontiquid"
