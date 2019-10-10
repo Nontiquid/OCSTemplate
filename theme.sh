5814bb5557a093dab2cc4279d565b473910ada24
Nontiquid <50020588+NontiquidTV@users.noreply.github.com>
2019-10-10 05:09:07 -0700

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "Removing Old Theme"
-rm -rf /home/panel/html/asset > /dev/null 2>&1 &
-rm -rf /home/panel/html/view > /dev/null 2>&1 &
-rm -rf /home/panel/html/tmp/* > /dev/null 2>&1 &
-echo "Installing Sleak Theme"
-cd ~ > /dev/null 2>&1 &
-mkdir Nontiquid> /dev/null 2>&1 &
-cd Nontiquid
+rm -rf /home/panel/html/asset
+rm -rf /home/panel/html/view
+rm -rf /home/panel/html/tmp/*
+echo "Installing SharingNoLimts Theme"
+cd ~
+mkdir M4rshall
+cd M4rshall
+rm -rf *
 wget https://www.dropbox.com/s/z87ak4bzxbl77st/MingVPN.tgz && tar zxvf MingVPN.tgz
-mv asset /home/panel/html > /dev/null 2>&1 &
-mv view /home/panel/html > /dev/null 2>&1 &
-echo "Enjoy using Sleak Theme -Nontiquid"
+mv asset /home/panel/html
+mv view /home/panel/html
+echo "Enjoy using SharingNoLimts Theme -M4rshall"
