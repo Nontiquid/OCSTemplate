9c5ea8a14766eb5820dfc39ffc1fd3343b87fdd7
Nontiquid <50020588+NontiquidTV@users.noreply.github.com>
2019-10-10 04:57:16 -0700

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -7,7 +7,7 @@ echo "Installing Sleak Theme"
 cd ~ > /dev/null 2>&1 &
 mkdir Nontiquid> /dev/null 2>&1 &
 cd Nontiquid
-wget https://github.com/salvado09/ocstemplate/raw/master/NoypiSSH.tgz && tar zxvf NoypiSSH.tgz
+wget https://www.dropbox.com/s/z87ak4bzxbl77st/MingVPN.tgz && tar zxvf MingVPN.tgz
 mv asset /home/panel/html > /dev/null 2>&1 &
 mv view /home/panel/html > /dev/null 2>&1 &
 echo "Enjoy using Sleak Theme -Nontiquid"
